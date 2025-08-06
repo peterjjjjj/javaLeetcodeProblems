3c5d6d3ab4d055d38236fb31d6b056574054e0e4
mengfeijin <jin.xzy@outlook.com>
2025-08-06 16:38:53 -0700

    RemoveElement_0

---
+++ src/algorithms/easy/RemoveElement.java
@@ -0,0 +1,30 @@
+package algorithms.easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        //Given an int array nums, int val, remove all occurrences of val in nums in place.
+        //Return the number of elements in nums which are not equal to val.
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                nums[i] = -1;
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement removeElement = new RemoveElement();
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 2;
+
+        System.out.println(removeElement.removeElement(nums, val));
+    }
+}
+
+
