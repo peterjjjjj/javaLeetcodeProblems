89f85f0ed9555e46c11339362c290447513a17a6
mengfeijin <jin.xzy@outlook.com>
2025-08-08 13:56:27 -0700

    CoinChange_2

--- src/algorithms/meduim/CoinChange.java
+++ src/algorithms/meduim/CoinChange.java
@@ -10,7 +10,12 @@ public class CoinChange {
             dp[i] = Integer.MAX_VALUE;
             for (int coin : coins) {
                 if (i - coin >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    if (dp[i - coin] == Integer.MAX_VALUE) {
+                        dp[i] = Integer.MAX_VALUE;
+                    }
+                    else {
+                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    }
                 }
             }
         }
@@ -24,6 +29,6 @@ public class CoinChange {
 
     public static void main(String[] args) {
         CoinChange test = new CoinChange();
-        System.out.println(test.coinChange(new int[]{1, 2, 5}, 11));
+        System.out.println(test.coinChange(new int[]{2}, 3));
     }
 }
