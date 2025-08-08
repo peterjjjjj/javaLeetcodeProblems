795985ceeb21c8b44a7cf2bb4a0e6f0b1b43b4c2
mengfeijin <jin.xzy@outlook.com>
2025-08-08 13:21:33 -0700

    MinCostClimbingStairs_0

---
+++ src/algorithms/easy/MinCostClimbingStairs.java
@@ -0,0 +1,23 @@
+package algorithms.easy;
+
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+
+        if (cost.length == 0) return 0;
+
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i < cost.length + 1; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[cost.length];
+    }
+
+    public static void main(String[] args) {
+        MinCostClimbingStairs test = new MinCostClimbingStairs();
+        System.out.println(test.minCostClimbingStairs(new int[] {10, 15, 20}));
+    }
+}
