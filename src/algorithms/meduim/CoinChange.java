4be20f2277f8c5b4059eba3ddfd3a6174f57e222
mengfeijin <jin.xzy@outlook.com>
2025-08-08 14:02:44 -0700

    CoinChange_3

--- src/algorithms/meduim/CoinChange.java
+++ src/algorithms/meduim/CoinChange.java
@@ -1,21 +1,18 @@
 package algorithms.meduim;
 
+import java.util.Arrays;
+
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         dp[0] = 0;
 
+        Arrays.fill(dp, 1, amount + 1, Integer.MAX_VALUE);
+
         for (int i = 1; i < amount + 1; i++) {
-            //Set the dp to inf.
-            dp[i] = Integer.MAX_VALUE;
             for (int coin : coins) {
-                if (i - coin >= 0) {
-                    if (dp[i - coin] == Integer.MAX_VALUE) {
-                        dp[i] = Integer.MAX_VALUE;
-                    }
-                    else {
-                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                    }
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
