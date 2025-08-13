9335b79857699859f0b6f1679beb4ddbf99d9172
mengfeijin <jin.xzy@outlook.com>
2025-08-13 15:25:11 -0700

    InvertBinaryTree_0

--- src/algorithms/easy/trees/InvertBinaryTree.java
+++ src/algorithms/easy/trees/InvertBinaryTree.java
@@ -16,12 +16,28 @@ public class InvertBinaryTree {
         return root;
     }
 
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree2(root.left);
+        TreeNode right = invertTree2(root.right);
+
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+
+        return root;
+    }
+
     public static void main(String[] args) {
         InvertBinaryTree test = new InvertBinaryTree();
         TreeNode tree = new TreeNode(1);
         tree.left = new TreeNode(2);
         tree.right = new TreeNode(3);
         System.out.println(test.invertTree(tree));
+        System.out.println(test.invertTree2(tree));
 
     }
 }
