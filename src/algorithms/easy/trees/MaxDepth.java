e987906866c2f54652f28e1bb1d2d318c6bc34bf
mengfeijin <jin.xzy@outlook.com>
2025-08-14 14:12:27 -0700

    MaxDepth_0

---
+++ src/algorithms/easy/trees/MaxDepth.java
@@ -0,0 +1,22 @@
+package algorithms.easy.trees;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        MaxDepth test = new MaxDepth();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(test.maxDepth(root));
+    }
+}
